f5fb63d88a6c72352ab1d1d188884482c910613c
Alon Krymgand <me@alon.kr>
2025-09-24 13:04:51 +0300

    added `libpcmseq__stereo_mixer__mix` signature to header

--- include/libpcmseq/mixer.h
+++ include/libpcmseq/mixer.h
@@ -53,4 +53,21 @@ libpcmseq__err_t
 libpcmseq__stereo_mixer__remove_voice(libpcmseq__stereo_mixer_t *mixer,
                                       libpcmseq__stereo_voice_id_t voice_id);
 
+/**
+ * @brief Mixes all active voices into the output buffer.
+ *
+ * Each frame in the output buffer is the average of all active voices' frames.
+ * TODO: make this more sophisticated (e.g., volume control, panning, etc.)
+ *
+ * @param[in] mixer Pointer to the mixer.
+ * @param[out] out_frames Pointer to the output buffer where mixed frames will
+ * be stored.
+ * @param[in] out_frames_num Number of frames to mix into the output buffer.
+ * @return LIBPCMSEQ__OK on success, or an error code on failure.
+ */
+libpcmseq__err_t
+libpcmseq__stereo_mixer__mix(libpcmseq__stereo_mixer_t *mixer,
+                             libpcmseq__stereo_frame_t *out_frames,
+                             size_t out_frames_num);
+
 #endif // LIBPCMSEQ_MIXER_H
