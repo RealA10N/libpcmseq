5bd55ed0e998c8872107f3e08abb3166d576cfde
Alon Krymgand <me@alon.kr>
2025-09-24 10:51:14 +0300

    added function signatures to header file

--- libpcmseq/include/libpcmseq.h
+++ libpcmseq/include/libpcmseq.h
@@ -34,3 +34,35 @@ typedef struct {
   // beginning. If false, playback stops at the end of the clip.
   bool loop;
 } libpcmseq__stereo_voice_t;
+
+/**
+ * @brief Initializes a stereo clip and allocate the required memory for its
+ * frames.
+ *
+ * @param[out] clip Pointer to the clip to initialize.
+ * @param[in] frames_num Number of frames to allocate.
+ * @param[in] sample_rate Sample rate in Hz.
+ * @return LIBPCMSEQ__OK on success, or an error code on failure.
+ */
+libpcmseq__err_t libpcmseq__init_stereo_clip(libpcmseq__stereo_clip_t *clip,
+                                             size_t frames_num,
+                                             libpcmseq__rate_hz_t sample_rate);
+/**
+ * @brief Deallocates the memory used by a stereo clip and resets its fields.
+ *
+ * @param[in,out] clip Pointer to the clip to delete.
+ */
+void libpcmseq__del_stereo_clip(libpcmseq__stereo_clip_t *clip);
+
+/**
+ * @brief Initializes a stereo clip with a square wave.
+ *
+ * @param[out] clip Pointer to the clip to initialize.
+ * @param[in] sample_rate Sample rate in Hz.
+ * @param[in] amplitude Amplitude of the square wave (positive value).
+ * @param[in] freq_hz Frequency of the square wave in Hz.
+ * @return LIBPCMSEQ__OK on success, or an error code on failure.
+ */
+libpcmseq__err_t libpcmseq__init_square_wave_stereo_clip(
+    libpcmseq__stereo_clip_t *clip, libpcmseq__rate_hz_t sample_rate,
+    libpcmseq__amplitude_t amplitude, libpcmseq__rate_hz_t freq_hz);
