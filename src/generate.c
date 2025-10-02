092bad84f8e71da289a89e5430c988357f036628
Alon Krymgand <me@alon.kr>
2025-10-02 13:09:53 +0300

    removed unused code

--- src/generate.c
+++ src/generate.c
@@ -2,9 +2,8 @@
 
 libpcm__err_t
 libpcm__generate_context__validate(const libpcm__generate_context_t *ctx) {
-  if (NULL == ctx || 0 == ctx->sample_rate || 0 == ctx->channels ||
-      NULL == ctx->float_to_sample || NULL == ctx->buffer.samples ||
-      0 == ctx->buffer.len) {
+  if (NULL == ctx || 0 == ctx->sample_rate || NULL == ctx->float_to_sample ||
+      NULL == ctx->buffer.samples || 0 == ctx->buffer.len) {
     return LIBPCM__ERR_INVALID_ARG;
   }
 
